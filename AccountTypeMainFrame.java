ad026e6c6d27b2211bb73724c6e9c1bbeb8dadd6
Joshua S <joshua.salom1996@gmail.com>
2016-11-25 20:36:48 -0800

    added GuestLoginFrame, ManagerLoginFrame. nonefunctional yet

--- AccountTypeMainFrame.java
+++ AccountTypeMainFrame.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
@@ -12,50 +14,72 @@ public class AccountTypeMainFrame
 {
 	public static void main(String args[])
 	{
-		//read from saved file
-		//each line of the file is formatted as so:
-		//user id, user name (example: John_Smith) (we input underscore between names when saving)
-		//list of each reservation detail under this user: room#, start time, end time
-		//room# such as "1" or "20" will be distinguishable from usernames, we can force users to create names >3 letters
-		
-		//TEMPORARY DATA STRUCTURE INITIALIZATION
+		// read from saved file
+		// each line of the file is formatted as so:
+		// user id, user name (example: John_Smith) (we input underscore between
+		// names when saving)
+		// list of each reservation detail under this user: room#, start time,
+		// end time
+		// room# such as "1" or "20" will be distinguishable from usernames, we
+		// can force users to create names >3 letters
+
+		// TEMPORARY DATA STRUCTURE INITIALIZATION
 		TreeMap<String, ArrayList<Reservation>> roomMap = new TreeMap<String, ArrayList<Reservation>>();
 		ArrayList<User> userList = new ArrayList<User>();
-		
+
 		HotelModel model = new HotelModel(roomMap, userList);
-		
+
 		JFrame frame = new JFrame();
 		frame.setSize(400, 140);
 		frame.setLayout(new BorderLayout());
-		
+
 		JLabel welcomeLabel = new JLabel("Welcome to the Hotel Reservation System!");
-		
+
 		JLabel label = new JLabel("Select Account Type");
 		JPanel labelPanel = new JPanel();
 		labelPanel.setLayout(new FlowLayout());
-		
+
 		JPanel welcomeLabelPanel = new JPanel();
 		welcomeLabelPanel.add(welcomeLabel);
-		
+
 		welcomeLabelPanel.add(welcomeLabel);
 		frame.add(welcomeLabelPanel, BorderLayout.NORTH);
-		
+
 		labelPanel.add(label);
 		frame.add(labelPanel, BorderLayout.CENTER);
-		
+
 		JButton guestButton = new JButton("Guest");
-		
-		
-		
+		guestButton.addActionListener(new ActionListener()
+				{
+
+					@Override
+					public void actionPerformed(ActionEvent arg0)
+					{
+						GuestLoginFrame guestLoginFrame = new GuestLoginFrame(model);
+						frame.dispose();
+					}
+			
+				});
+
 		JButton managerButton = new JButton("Manager");
-		
+		managerButton.addActionListener(new ActionListener()
+				{
+
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						ManagerLoginFrame managerLoginFrame = new ManagerLoginFrame(model);
+						frame.dispose();
+					}
+			
+				});
+
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout());
 		buttonPanel.add(guestButton);
 		buttonPanel.add(managerButton);
 		frame.add(buttonPanel, BorderLayout.SOUTH);
-		
-		
+
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
---
+++ GuestLoginFrame.java
@@ -0,0 +1,82 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GuestLoginFrame
+{
+	public GuestLoginFrame(HotelModel model)
+	{
+		JFrame frame = new JFrame();
+		frame.setSize(400, 260);
+		frame.setLayout(new BorderLayout());
+
+		JLabel welcomeLabel = new JLabel("Guest: Sign In or Sign Up");
+		JPanel welcomeLabelPanel = new JPanel();
+		welcomeLabelPanel.add(welcomeLabel);
+		frame.add(welcomeLabelPanel, BorderLayout.NORTH);
+		
+		JPanel signInPanel = new JPanel();
+		signInPanel.setLayout(new BoxLayout(signInPanel, BoxLayout.PAGE_AXIS));
+		
+		JLabel signInTitle = new JLabel("Sign In to Existing Guest Account");
+		JPanel signInTitlePanel = new JPanel();
+		signInTitlePanel.setLayout(new FlowLayout());
+		signInTitlePanel.add(signInTitle);
+		signInPanel.add(signInTitlePanel);
+		
+		JLabel signInPrompt = new JLabel("User ID: ");
+		JTextField signInTextField = new JTextField(12);
+		JPanel signInContent = new JPanel();
+		signInContent.add(signInPrompt);
+		signInContent.add(signInTextField);
+		signInPanel.add(signInContent);
+		
+		JButton signInButton = new JButton("Sign In");
+		signInContent.add(signInButton);
+		//TODO
+		
+		frame.add(signInPanel, BorderLayout.CENTER);
+		
+		JPanel signUpPanel = new JPanel();
+		signUpPanel.setLayout(new BoxLayout(signUpPanel, BoxLayout.PAGE_AXIS));
+		
+		JLabel signUpTitle = new JLabel("Sign Up for New Guests");
+		JPanel signUpTitlePanel = new JPanel();
+		signUpTitlePanel.setLayout(new FlowLayout());
+		signUpTitlePanel.add(signUpTitle);
+		signUpPanel.add(signUpTitlePanel);
+		
+		JLabel signUpPrompt1 = new JLabel("New User ID: ");
+		JTextField signUpTextField1 = new JTextField(12);
+		JPanel signUpContent1 = new JPanel();
+		signUpContent1.add(signUpPrompt1);
+		signUpContent1.add(signUpTextField1);
+		signUpPanel.add(signUpContent1);
+		
+		JLabel signUpPrompt2 = new JLabel("Full Name: ");
+		JTextField signUpTextField2 = new JTextField(12);
+		JPanel signUpContent2 = new JPanel();
+		signUpContent2.add(signUpPrompt2);
+		signUpContent2.add(signUpTextField2);
+		signUpPanel.add(signUpContent2);
+		
+		JButton signUpButton = new JButton("Sign Up");
+		signUpPanel.add(signUpButton);
+		
+		JLabel blank = new JLabel(" ");
+		signUpPanel.add(blank);
+		
+		frame.add(signUpPanel, BorderLayout.SOUTH);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
