3d9f0d9757ef1da312d42e47061506f489f8f130
Joshua S <joshua.salom1996@gmail.com>
2016-11-25 18:52:45 -0800

    renamed AccountTypeFrame -> AccountTypeMainFrame; added nonfunctional buttons

--- AccountTypeMainFrame.java
+++ AccountTypeMainFrame.java
@@ -1,10 +1,12 @@
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class AccountTypeFrame
+public class AccountTypeMainFrame
 {
 	public static void main(String args[])
 	{
@@ -17,17 +19,34 @@ public class AccountTypeFrame
 		
 		
 		JFrame frame = new JFrame();
-		frame.setSize(400, 200);
+		frame.setSize(400, 140);
 		frame.setLayout(new BorderLayout());
 		
+		JLabel welcomeLabel = new JLabel("Welcome to the Hotel Reservation System!");
+		
 		JLabel label = new JLabel("Select Account Type");
-		frame.add(label, BorderLayout.NORTH);
+		JPanel labelPanel = new JPanel();
+		labelPanel.setLayout(new FlowLayout());
+		
+		JPanel welcomeLabelPanel = new JPanel();
+		welcomeLabelPanel.add(welcomeLabel);
+		
+		welcomeLabelPanel.add(welcomeLabel);
+		frame.add(welcomeLabelPanel, BorderLayout.NORTH);
+		
+		labelPanel.add(label);
+		frame.add(labelPanel, BorderLayout.CENTER);
 		
 		JButton guestButton = new JButton("Guest");
-		frame.add(guestButton, BorderLayout.WEST);
 		
 		JButton managerButton = new JButton("Manager");
-		frame.add(managerButton, BorderLayout.EAST);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.add(guestButton);
+		buttonPanel.add(managerButton);
+		frame.add(buttonPanel, BorderLayout.SOUTH);
+		
 		
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
