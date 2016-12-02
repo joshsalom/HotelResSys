7686997a300adb5013f671b04e901c21c3e9ecbd
alexliu16 <alexliu@Alexs-MacBook-Air.local>
2016-12-01 16:41:40 -0800

    GuestCheckFrame makes sure that inputted dates are formatted correctly and enforces all constraints properly

--- GuestCheckFrame.java
+++ GuestCheckFrame.java
@@ -2,11 +2,17 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -66,11 +72,17 @@ public class GuestCheckFrame
 					@Override
 					public void actionPerformed(ActionEvent arg0)
 					{
-						model.updateCurrentStart(inTextField.getText());
-						model.updateCurrentEnd(outTextField.getText());
-						model.updateCurrentRoomPref("economy");
-						GuestViewFrame newGVF = new GuestViewFrame(model);
-						frame.dispose();
+						String startDate = inTextField.getText();
+						String endDate = outTextField.getText();
+						if(checkValidDate(startDate, endDate)){
+							model.updateCurrentStart(startDate);
+							model.updateCurrentEnd(endDate);
+							model.updateCurrentRoomPref(RoomType.ECONOMY);
+							GuestViewFrame newGVF = new GuestViewFrame(model);
+							frame.dispose();
+						}
+						else
+							JOptionPane.showMessageDialog(null, "Please enter a valid date");
 					}
 			
 				});
@@ -83,11 +95,17 @@ public class GuestCheckFrame
 					@Override
 					public void actionPerformed(ActionEvent arg0)
 					{
-						model.updateCurrentStart(inTextField.getText());
-						model.updateCurrentEnd(outTextField.getText());
-						model.updateCurrentRoomPref("luxury");
-						GuestViewFrame newGVF = new GuestViewFrame(model);
-						frame.dispose();
+						String startDate = inTextField.getText();
+						String endDate = outTextField.getText();
+						if(checkValidDate(startDate, endDate)){
+							model.updateCurrentStart(startDate);
+							model.updateCurrentEnd(endDate);
+							model.updateCurrentRoomPref(RoomType.LUXURY);
+							GuestViewFrame newGVF = new GuestViewFrame(model);
+							frame.dispose();
+						}	
+						else
+							JOptionPane.showMessageDialog(null, "Please enter a valid date");
 					}
 			
 				});
@@ -99,4 +117,60 @@ public class GuestCheckFrame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
+	
+	//checks that the dates that are entered are valid
+	public boolean checkValidDate(String startDate, String endDate){
+		//check to see if date entered in format MM/DD/YYYY
+		if (startDate.length() != 10 || endDate.length() != 10)
+			return false;
+		//check to make sure they used "/" to separate dates
+		if(!startDate.substring(2, 3).equals("/") || !startDate.substring(5, 6).equals("/") 
+				|| !endDate.substring(2, 3).equals("/") || !endDate.substring(5, 6).equals("/"))
+			return false;
+		// get month, day, year information
+		String[] startDateInfo = startDate.split("/", -1);
+		String[] endDateInfo = endDate.split("/", -1);
+		String startMonth = startDateInfo[0];
+		String startDay = startDateInfo[1];
+		String startYear = startDateInfo[2];
+		String endMonth = endDateInfo[0];
+		String endDay = endDateInfo[1];
+		String endYear = endDateInfo[2];
+		// check to make sure input doesn't contain letters
+		for (String s : startDateInfo)
+			if (!s.matches("^-?\\d+$"))
+				return false;
+		for (String s : endDateInfo)
+			if (!s.matches("^-?\\d+$"))
+				return false;
+		//parse into ints
+		int sMonth = Integer.parseInt(startMonth);
+		int sDay = Integer.parseInt(startDay);
+		int sYear = Integer.parseInt(startYear);
+		int eMonth = Integer.parseInt(endMonth);
+		int eDay = Integer.parseInt(endDay);
+		int eYear = Integer.parseInt(endYear);
+		//check to make sure that month is 1-12 and day is 1-31
+		if((sMonth < 1 || sMonth > 12) || (eMonth < 1 || eMonth > 12) 
+				|| ((sDay < 1 || sDay > 31) || (eDay < 1 || eDay > 31)))
+			return false;
+		//check to make sure end date and start date are after current date
+		GregorianCalendar cal = new GregorianCalendar();
+		GregorianCalendar otherCal = new GregorianCalendar(sYear, sMonth - 1, sDay);
+		GregorianCalendar thirdCal = new GregorianCalendar(eYear, eMonth - 1, eDay);
+		if(cal.compareTo(otherCal) > 0 || cal.compareTo(thirdCal) >0)
+			return false;
+		// check to make sure end date is after the start date
+		if(otherCal.getTime().compareTo(thirdCal.getTime()) >= 0)
+			return false;
+		//check that stay doesn't extend past 60 days
+		if (daysBetween(otherCal.getTime(), thirdCal.getTime()) > 60)
+			return false;
+		return true;
+	}
+	
+	//helper method
+	private int daysBetween(Date d1, Date d2){
+		return (int)( (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
+ }
 }
