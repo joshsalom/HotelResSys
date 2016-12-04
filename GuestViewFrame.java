0fc59955326bc3d715be8bd60dfa87ce890ae527
alexliu16 <alexliu@Alexs-MacBook-Air.local>
2016-12-04 12:38:20 -0800

    Creating reservations now functions properly

--- GuestViewFrame.java
+++ GuestViewFrame.java
@@ -1,30 +1,31 @@
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class GuestViewFrame
 {
 	private HotelModel model;
+	private JTextArea roomView;
+	private ArrayList<Room> validRooms;
 	
 	public GuestViewFrame(HotelModel hotelModel)
 	{
 		this.model = hotelModel;
-		
+		validRooms = new ArrayList<Room>();
 		JFrame frame = new JFrame();
 		frame.setSize(800, 300);
 		frame.setLayout(new BorderLayout());
 		
-		JTextArea roomView = new JTextArea(10, 10);
+		roomView = new JTextArea(10, 10);
 		roomView.setText("Test");
 		roomView.setEditable(false);
-		frame.add(roomView, BorderLayout.CENTER);
+		JScrollPane scroller = new JScrollPane(roomView);
+		updateRoomView();
+		frame.add(scroller, BorderLayout.CENTER);
 		
 		JPanel contentPanel = new JPanel();
 		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS));
@@ -40,9 +41,57 @@ public class GuestViewFrame
 		buttonPanel.setLayout(new FlowLayout());
 		
 		JButton confirmButton = new JButton("Confirm");
+		confirmButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int roomNumber = 0;
+				//make sure that the number entered is an integer
+				try{
+					roomNumber = Integer.parseInt(inputTextField.getText());
+				}
+				catch(NumberFormatException exception){
+					JOptionPane.showMessageDialog(null, "Please enter a valid room number");
+					return;
+				}
+				//check that number entered is an available room
+				Reservation reservation = new Reservation(null, model.getCurrentStart(), model.getCurrentEnd(), model.getCurrentUser());
+				boolean enteredValidRoom = false;
+				for(Room r: validRooms)
+					if(r.getRoomNumber() == roomNumber){
+						reservation.setRoom(r);
+						model.getRoomMap().get(r).add(reservation);
+						Guest g = (Guest)model.getCurrentUser();
+						g.addReservation(reservation);
+						enteredValidRoom = true;
+					}
+				if(!enteredValidRoom)
+					JOptionPane.showMessageDialog(null, "Please enter a valid room number");
+				else
+					try {
+						JOptionPane.showMessageDialog(null, "Reservation made");
+						model.storeHotelInformation();
+						updateRoomView();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+			}
+				
+			
+		});
+		
 		buttonPanel.add(confirmButton);
 		
 		JButton moreButton = new JButton("More Reservations?");
+		moreButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				GuestCheckFrame guestCheckFrame = new GuestCheckFrame(model);
+				frame.dispose();
+			}
+			
+		});
 		buttonPanel.add(moreButton);
 		
 		JButton doneButton = new JButton("Done");
@@ -58,5 +107,31 @@ public class GuestViewFrame
 		
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void updateRoomView(){
+		String rooms = "Available Rooms " + model.getCurrentStart() + "-" + model.getCurrentEnd() + "\n\n";
+		Reservation reservation = new Reservation(null, model.getCurrentStart(), model.getCurrentEnd(), model.getCurrentUser());
+		ArrayList<Reservation> reservations = null;
+		validRooms = new ArrayList<Room>();
+		boolean addRoom = true;
+		for(Room r: model.getRoomMap().keySet()){
+			//check to make sure room is correct preference
+			if(r.getRoomType() != model.getCurrentRoomPref())
+				addRoom = false;
+			reservations = model.getRoomMap().get(r);
+			//checks to see if reservation has a conflict with previous reservations
+			for(Reservation res: reservations)
+				if(reservation.checkHasConflict(res))
+					addRoom = false;
+			if(addRoom){
+				validRooms.add(r);
+				rooms += r.getRoomNumber() + "\n";
+			}	
+			addRoom = true;
+		}	
+		if(validRooms.isEmpty())
+			rooms = "No available rooms for " + model.getCurrentStart() + "-" + model.getCurrentEnd();
+		roomView.setText(rooms);
 	}
 }
